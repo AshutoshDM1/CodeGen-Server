595eabb6a6146a0958a0b882be158fab160a280d
AshutoshDM1 <downlodemaster1@gmail.com>
2024-12-20 23:14:49 +0530

    prompt engineering

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import Anthropic from "@anthropic-ai/sdk";
 import cors from "cors";
+import { BASE_PROMPT, getSystemPrompt } from "./helper/prompts";
+import { ContentBlock, TextBlock } from "@anthropic-ai/sdk/resources";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
 
 dotenv.config();
 
@@ -24,9 +28,8 @@ app.get("/chat", async (req, res) => {
     const stream: any = await anthropic.messages.create({
       model: "grok-beta",
       stream: true,
-      max_tokens: 3000,
-      system:
-        "you are a helpful assistant who write code in js you will only give code anything else is not allowed",
+      max_tokens: 8000,
+      system: getSystemPrompt(),
       messages: [
         {
           role: "user",
@@ -46,6 +49,48 @@ app.get("/chat", async (req, res) => {
   } catch (error: any) {
     res.status(500).json({ msg: error.message });
   }
+});
+
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  const response = await anthropic.messages.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "claude-3-5-sonnet-20241022",
+    max_tokens: 200,
+    system:
+      "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
+  });
+
+  const answer = (response.content[0] as TextBlock).text; // react or node
+  if (answer == "react") {
+    res.json({
+      prompts: [
+        BASE_PROMPT,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [reactBasePrompt],
+    });
+    return;
+  }
+
+  if (answer === "node") {
+    res.json({
+      prompts: [
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [nodeBasePrompt],
+    });
+    return;
+  }
+
+  res.status(403).json({ message: "You cant access this" });
+  return;
 });
 
 app.listen(port, () => {
