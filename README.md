d383efba4116ab09a1f420c700414babcb44ae13
AshutoshDM1 <downlodemaster1@gmail.com>
2024-12-21 23:45:14 +0530

    Fix

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CodeGen-Server
 
 # Not completed yet
+
+**Site Link** - https://codegen-server-yuxj.onrender.com
+
+**Github Link** - https://github.com/yuxj/codegen-server
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port: Number = 4000;
 const API = process.env.GROK_API;
 
@@ -20,11 +21,12 @@ const anthropic = new Anthropic({
 });
 
 app.get("/", async (req, res) => {
-  res.status(300).json({ msg: "Welcome to CodeGen Server" });
+  res.status(200).json({ msg: "Welcome to CodeGen Server" });
 });
 
-app.get("/chat", async (req, res) => {
+app.post("/chat", async (req, res) => {
   try {
+    const prompt = req.body.prompt;
     const stream: any = await anthropic.messages.create({
       model: "grok-beta",
       stream: true,
@@ -33,7 +35,7 @@ app.get("/chat", async (req, res) => {
       messages: [
         {
           role: "user",
-          content: "write a normal code to sum two numbers in js ",
+          content: prompt,
         },
       ],
     });
@@ -52,45 +54,51 @@ app.get("/chat", async (req, res) => {
 });
 
 app.post("/template", async (req, res) => {
-  const prompt = req.body.prompt;
+  try {
+    const prompt = req.body.prompt;
 
-  const response = await anthropic.messages.create({
-    messages: [
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
-    model: "claude-3-5-sonnet-20241022",
-    max_tokens: 200,
-    system:
-      "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
-  });
-
-  const answer = (response.content[0] as TextBlock).text; // react or node
-  if (answer == "react") {
-    res.json({
-      prompts: [
-        BASE_PROMPT,
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+    const response = await anthropic.messages.create({
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
       ],
-      uiPrompts: [reactBasePrompt],
+      model: "grok-beta",
+      max_tokens: 200,
+      system:
+        "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
     });
-    return;
-  }
 
-  if (answer === "node") {
-    res.json({
-      prompts: [
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
-      ],
-      uiPrompts: [nodeBasePrompt],
-    });
-    return;
-  }
 
-  res.status(403).json({ message: "You cant access this" });
-  return;
+
+    const answer = (response.content[0] as TextBlock).text; // react or node
+    if (answer == "react") {
+      res.json({
+        prompts: [
+          BASE_PROMPT,
+          `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        ],
+        uiPrompts: [reactBasePrompt],
+      });
+      return;
+    }
+
+    if (answer === "node") {
+      res.json({
+        prompts: [
+          `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        ],
+        uiPrompts: [nodeBasePrompt],
+      });
+      return;
+    }
+
+    res.status(403).json({ message: "You cant access this" });
+    return;
+  } catch (error: any) {
+    res.status(500).json({ msg: error.message });
+  }
 });
 
 app.listen(port, () => {
